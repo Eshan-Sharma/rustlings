281be02d0bf9838f6cecf77f6e68ba6d8bb74b1a
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-05-10 15:29:10 +0530

    type conversions on from str
    come back to these conversions, they are tough ngl

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -48,6 +46,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let fields: Vec<&str> = s.split(',').collect();
+        if fields.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = fields[0].trim();
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let age_result = fields[1].trim().parse::<usize>();
+        let age = match age_result {
+            Ok(age) => age,
+            Err(err) => return Err(ParsePersonError::ParseInt(err)),
+        };
+        Ok(Person {
+            name: name.to_string(),
+            age,
+        })
     }
 }
 
